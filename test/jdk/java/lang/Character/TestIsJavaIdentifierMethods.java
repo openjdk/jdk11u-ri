0c9539752be937ca2107912da8c83e2183f47426
Naoto Sato <naoto.sato@oracle.com>
2023-02-16 00:13:13 +0000

    Fixed test headers

--- test/jdk/java/lang/Character/TestIsJavaIdentifierMethods.java
+++ test/jdk/java/lang/Character/TestIsJavaIdentifierMethods.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,7 +25,7 @@
  * @test
  * @summary Test behavior of isJavaIdentifierXX, isJavaLetter, and
  *  isJavaLetterOrDigit methods for all code points.
- * @bug 8218915
+ * @bug 8218915 8301401
  */
 
 public class TestIsJavaIdentifierMethods {
